5a2fa6272e9800a70739a76e0dd8d7d2075bca94
Zachary Sailer <zachsailer@gmail.com>
2015-04-10 13:05:50 -0700

    Form into small package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='infection',
+      version='0.1',
+      description='Simulation of Infection game by Andrea Loes.',
+      author='Zach Sailer',
+      author_email='zachsailer@gmail.com',
+      url='',
+      packages=['names'],
+)
