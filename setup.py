0b68f724745ce6b07fc209edca4810939afb5bbb
Zachary Sailer <zachsailer@gmail.com>
2015-04-10 13:22:07 -0700

    Fixing setup

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
+# For installation  
 setup(name='infection',
       version='0.1',
       description='Simulation of Infection game by Andrea Loes.',
       author='Zach Sailer',
       author_email='zachsailer@gmail.com',
       url='',
-      packages=['names'],
+      packages=['infection']
+      install_requires=['names'],
 )
